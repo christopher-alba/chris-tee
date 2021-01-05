2554efe755818a55a0b5314dc874d920425f2e0c
Christopher <calb200@aucklanduni.ac.nz>
2021-01-06 08:20:56 +1300

    add add to cart button in product page

--- client/pages/product.js
+++ client/pages/product.js
@@ -2,9 +2,24 @@ import React, { useState } from 'react';
 import { useQuery } from '@apollo/client';
 import { GET_PRODUCT, AUTHENTICATE } from '../ApolloClient/queries';
 import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
 import EditProductDialog from '../components/editProductDialog';
+import { makeStyles } from '@material-ui/core';
 
+const useStyles = makeStyles({
+  productPageWrapper: {
+    marginTop: '100px',
+    marginBottom: '100px',
+  },
+  productImage: {
+    height: '500px',
+  },
+  editProductButton: {
+    marginRight: '20px',
+  },
+});
 const Product = () => {
+  const classes = useStyles();
   const id = window.location.hash.split('/')[2];
   const {
     loading: productLoading,
@@ -44,24 +59,27 @@ const Product = () => {
   } = productData.product;
   return (
     <>
-      <div>
-        <h1>welcome to the product page</h1>
-        <h2>{name}</h2>
-        <img src={image} />
+      <Container className={classes.productPageWrapper}>
+        <h1>Welcome to the product page</h1>
+        <h2>Currently Viewing {name}</h2>
+        <img src={image} className={classes.productImage} />
         <p>{description}</p>
         <p>Orientation: {orientation}</p>
         <p>Clothing Type: {clothingType}</p>
         <h4>NZD${price}</h4>
         {adminPermission && (
           <Button
+            className={classes.editProductButton}
             onClick={() => {
               setEditProductOpen(true);
             }}
+            variant="outlined"
           >
             Edit Product
           </Button>
         )}
-      </div>
+        <Button variant="outlined">Add to Cart</Button>
+      </Container>
       <EditProductDialog
         handleEditProductClose={handleEditProductClose}
         editProductOpen={editProductOpen}
