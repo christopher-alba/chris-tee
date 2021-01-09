93bc01ad50635b7398afc87e8bb9f3ddd5eff507
Christopher <calb200@aucklanduni.ac.nz>
2021-01-09 15:36:17 +1300

    style the cart page

--- client/pages/cart.js
+++ client/pages/cart.js
@@ -3,8 +3,46 @@ import { useQuery, useMutation } from '@apollo/client';
 import { AUTHENTICATE, GET_CART } from '../ApolloClient/queries';
 import { UPDATE_CART } from '../ApolloClient/mutations';
 import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
 import { Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  cartHeader: {
+    marginTop: '100px',
+  },
+  productImage: {
+    width: '250px',
+  },
+  productWrapper: {
+    padding: '50px',
+    margin: '0 auto',
+    width: '250px',
+    '&:hover': {
+      cursor: 'pointer',
+    },
+  },
+  productsWrapper: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    paddingTop: '50px',
+    paddingBottom: '50px',
+  },
+  emptyCart: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '90vh',
+    width: '100%',
+  },
+  emptyCartInner: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+});
 const Cart = () => {
+  const classes = useStyles();
   const { loading: authLoading, error: authError, data: authData } = useQuery(
     AUTHENTICATE
   );
@@ -24,9 +62,25 @@ const Cart = () => {
     return <div>Loading...</div>;
   }
   const { products } = cartData.cart;
+
+  if (products.length === 0) {
+    return (
+      <Box className={classes.emptyCart}>
+        <Box className={classes.emptyCartInner}>
+          <h1>Your Cart is currently empty</h1>
+          <Button variant="outlined" href="#/shop">
+            View Items in Shop
+          </Button>
+        </Box>
+      </Box>
+    );
+  }
+
   return (
-    <Box>
-      <h1>Welcome to your Cart {authData.me.username}</h1>
+    <Container>
+      <h1 className={classes.cartHeader}>
+        Welcome to your Cart {authData.me.username}
+      </h1>
       {products.length > 0 && (
         <Button
           onClick={() => {
@@ -95,43 +149,85 @@ const Cart = () => {
           </Button>
         </>
       )}
-      {products.map((product) => {
-        const {
-          id,
-          name,
-          description,
-          image,
-          orientation,
-          price,
-          clothingType,
-          size,
-        } = product;
-        return (
-          <Box
-            key={id}
-            onClick={() => {
-              setSelectedProduct(id);
-            }}
-            style={{ border: selectedProduct === id ? '3px solid black' : '' }}
-          >
-            <h2>{name}</h2>
-            <p>{description}</p>
-            <img
-              src={image}
-              alt="product"
-              onError={(image) => {
-                image.target.src =
-                  'https://www.k3ma.com/wp-content/uploads/2017/04/default-image.jpg';
+      <Box className={classes.productsWrapper}>
+        {products.map((product) => {
+          const {
+            id,
+            name,
+            description,
+            image,
+            orientation,
+            price,
+            clothingType,
+            size,
+          } = product;
+          return (
+            <Box
+              className={classes.productWrapper}
+              key={id}
+              onClick={() => {
+                setSelectedProduct(id);
               }}
-            />
-            <p>Orientation: {orientation}</p>
-            <p>Clothing Type: {clothingType}</p>
-            <p>Size: {size}</p>
-            <h2>NZD${price}</h2>
-          </Box>
-        );
-      })}
-    </Box>
+              style={{
+                border: selectedProduct === id ? '3px solid black' : '',
+              }}
+            >
+              <h2>{name}</h2>
+              <p>{description}</p>
+              <img
+                className={classes.productImage}
+                src={image}
+                alt="product"
+                onError={(image) => {
+                  image.target.src =
+                    'https://www.k3ma.com/wp-content/uploads/2017/04/default-image.jpg';
+                }}
+              />
+              <p>Orientation: {orientation}</p>
+              <p>Clothing Type: {clothingType}</p>
+              <p>Size: {size}</p>
+              <h2>NZD${price}</h2>
+              <Button
+                onClick={async () => {
+                  const updatedProducts = products.filter((product) => {
+                    return product.id !== id;
+                  });
+                  const newCartProducts = updatedProducts.map((product) => {
+                    return {
+                      name: product.name,
+                      price: product.price,
+                      description: product.description,
+                      image: product.image,
+                      orientation: product.orientation,
+                      clothingType: product.clothingType,
+                      size: product.size,
+                    };
+                  });
+                  await updateCart({
+                    variables: {
+                      username: authData.me.username,
+                      products: [...newCartProducts],
+                    },
+                    refetchQueries: [
+                      {
+                        query: GET_CART,
+                        variables: {
+                          username: authData.me.username,
+                        },
+                      },
+                    ],
+                  });
+                  setSelectedProduct('');
+                }}
+                variant="outlined"
+              >
+                remove from cart
+              </Button>
+            </Box>
+          );
+        })}
+      </Box>
+    </Container>
   );
 };
 
